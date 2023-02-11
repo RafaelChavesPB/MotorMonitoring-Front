f0b30b49f1bf0ab66917f768670bf24ddbceea1b
rafael <rafaelchavesaec@gmail.com>
2023-02-11 17:25:23 -0300

    Initial

--- src/components/DataChart.vue
+++ src/components/DataChart.vue
@@ -1,30 +1,69 @@
 <template>
-    <div>
-        <div>
-            {{fakedata}}
-        </div>
-        <v-btn @click="update"> Append data </v-btn>
-    </div>
+  <div>
+    <Bar id="my-chart-id" :options="chartOptions" :data="mydata" />
+  </div>
 </template>
 
 <script>
+import io from "socket.io-client";
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
 export default {
-    name: 'DataChart',
-    data(){
-        return {
-            fakedata: [],
-        }
+  name: "DataChart",
+  components: { Bar },
+  data() {
+    return {
+      mydata: {
+        datasets: [{ data: [1, 2, 3, 4, 5, 6] }],
+        label: ["a", "b", "c", "d", "e", "f"],
+      },
+      socket: {},
+      chartOptions: {
+        responsive: true,
+      },
+    };
+  },
+  created() {
+    this.socket = io("http://localhost:3000");
+  },
+  mounted() {
+    this.socket.on("new data", (params) => {
+      // this.update(params);
+    });
+  },
+  // computed:{
+  //   chartData(){
+  //     console.log(this.mydata.datasets[0].data)
+  //     return this.mydata
+  //   }
+  // },
+  methods: {
+    update(params) {
+      this.mydata.datasets[0].data.push(params.number);
+      this.mydata.label.push(params.time);
+      if (this.mydata.datasets[0].data.length > 40) {
+        this.mydata.datasets[0].data.splice(0, 1);
+        this.mydata.label.splice(0, 1);
+      }
     },
-    methods:{
-        update(){
-            this.fakedata.push(this.getRandomNumber())
-            if(this.fakedata.length > 40)
-                this.fakedata.splice(0, 1)
-        },
-        getRandomNumber(){
-            const max = 440;
-            return Math.floor(Math.random()*(max))
-        }
-    }
-}
+  },
+};
 </script>
